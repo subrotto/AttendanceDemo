6daacdc36c1425b78ccd9db1047336278f2bc9c6
subrotto <subrotokumargunopu@gmail.com>
2024-02-10 22:56:48 +0600

    latest2

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import SpecificClass from '../SpecificClass/SpecificClass';
 import ReactDatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
 const ClassList = () => {
     const [classes,setClasses]=useState([]);
@@ -10,21 +11,22 @@ const ClassList = () => {
     const [selectedDate, setSelectedDate] = useState(null); 
     const handleClassAdd=()=>{
         const className=classRef.current.value;
-
+       const classDate=new Date(selectedDate)
+        console.log(classDate)
         fetch(`http://localhost:5000/addClass/${courseId}`,{
             method:'POST',
             headers: {
                 
                 'Content-Type': 'application/json'
               },
-              body:JSON.stringify({date: selectedDate})
+              body:JSON.stringify({date: classDate})
         })
         .then(res=>res.json())
         .then(data=>{
             if(data.insertedId){
                 
         }});
-
+            setSelectedDate('');
         classRef.current.value='';
     }
 
@@ -40,24 +42,23 @@ const ClassList = () => {
     
     
     
-
     return (
         <div>
-             <input type="text" name="" id="" placeholder='Class Date' ref={classRef}/>
+            <input type="text" name="" id="" placeholder='Class Date' ref={classRef}/>
             <button onClick={handleClassAdd}>ADD CLASS</button>
-            <ReactDatePicker // Datepicker component
-                selected={selectedDate} // Set the selected date
-                onChange={date => setSelectedDate(date)} // Handle date selection
-                dateFormat="yyyy/MM/dd" // Format of the selected date
-                placeholderText="Select class date" // Placeholder text for the input field
+            <ReactDatePicker
+                selected={selectedDate}
+                onChange={date => setSelectedDate(date)}
+                dateFormat="dd/MM/yyyy" // Change the date format here
+                placeholderText="Select class date"
             />
-
+    
             {
-               classes.map(classId=><SpecificClass key={classId} classId={classId}></SpecificClass>) 
+                classes.map(classId=><SpecificClass key={classId} classId={classId}></SpecificClass>) 
             }
-
         </div>
-    );
+    )
+    ;
 };
 
 export default ClassList;
